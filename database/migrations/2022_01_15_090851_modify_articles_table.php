2f1f8b5314f84ddc4cad5b2d7cda084a701a2777
mihai <botanmazuro@gmail.com>
2022-01-15 11:21:03 +0200

    modily article table, modify graphql mutations

---
+++ database/migrations/2022_01_15_090851_modify_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('image')->default('neon.jpeg')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('image')->change();
+        });
+    }
+}
