ec887d2778e034cf3ddb22c10b6478c367f4d595
mihai <botanmazuro@gmail.com>
2022-01-20 11:49:13 +0200

    add guards

---
+++ app/Policies/GoalPolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Policies;
+
+use App\Models\Goal;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GoalPolicy
+{
+    use HandlesAuthorization;
+
+    public function view(User $user, Goal $goal): bool
+    {
+        return $user->id === $goal->user_id;
+    }
+
+    public function update(User $user, Goal $goal): bool
+    {
+        return $user->id === $goal->user_id;
+    }
+
+    public function delete(User $user, Goal $goal): bool
+    {
+        return $user->id === $goal->user_id;
+    }
+}
