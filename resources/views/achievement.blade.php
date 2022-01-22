bd15177dba09d9a3d8403dc9fbfa9e3f16cdc804
mihai <botanmazuro@gmail.com>
2022-01-23 00:05:51 +0200

    add achievements, modify readme

--- resources/views/achievement.blade.php
+++ resources/views/achievement.blade.php
@@ -22,8 +22,10 @@
             </div>
         </nav>
         <div id="achievement">
-            <div id="content">
-                achievements
+            <div id="content" style="margin-left: 20px;">
+                <img src="{{ asset('images/icons8-climbing-skin-type-1-48.png') }}">
+                <img src="{{ asset('images/icons8-heart-rate-monitor-64.png') }}">
+                <img src="{{ asset('images/icons8-место-хранения-64.png') }}">
             </div>
         </div>
     </section>
